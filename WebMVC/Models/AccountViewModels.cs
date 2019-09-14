7d160ae7e631a28b965734201203cf780aa23d8c
Oliver Kicks <kicks.oliver@gmail.com>
2019-09-14 10:50:42 -0400

    Added user/admin roles

--- WebMVC/Models/AccountViewModels.cs
+++ WebMVC/Models/AccountViewModels.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace WebMVC.Models
@@ -49,9 +50,8 @@ namespace WebMVC.Models
     public class LoginViewModel
     {
         [Required]
-        [Display(Name = "Email")]
-        [EmailAddress]
-        public string Email { get; set; }
+        [Display(Name = "User Name")]
+        public string UserName { get; set; }
 
         [Required]
         [DataType(DataType.Password)]
@@ -65,9 +65,18 @@ namespace WebMVC.Models
     public class RegisterViewModel
     {
         [Required]
+        [Display(Name = "User Roles")]
+        [DefaultValue("User")]
+        public string UserRoles { get; set; }
+
+        [Required]
         [EmailAddress]
         [Display(Name = "Email")]
         public string Email { get; set; }
+
+        [Required]
+        [Display(Name = "User Name")]
+        public string UserName { get; set; }
 
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
